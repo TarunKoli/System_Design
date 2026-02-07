220ffdfcef2208ac1432505cad16fbc34a91c876
Tarun Koli <tarunkoli@Taruns-MacBook-Air-2.local>
2026-02-08 01:57:53 +0530

    Command Pattern Added

---
+++ CommandPattern/BadCode/Program.cs
@@ -0,0 +1,63 @@
+﻿public interface IButton
+{
+    void Action();
+}
+
+public class BoldButton : IButton
+{
+    private TextEditor textEditor;
+    public BoldButton(TextEditor textEditor)
+    {
+        this.textEditor = textEditor;
+    }
+
+    public void Action()
+    {
+        textEditor.BoldText();
+    }
+}
+
+public class ItalicizeButton : IButton
+{
+    private TextEditor textEditor;
+
+    public ItalicizeButton(TextEditor textEditor)
+    {
+        this.textEditor = textEditor;
+    }
+
+    public void Action()
+    {
+        textEditor.ItalicizeText();
+    }
+}
+
+
+public class TextEditor
+{
+    public void BoldText()
+    {
+        Console.WriteLine("This text is bolded....");
+    }
+
+    public void ItalicizeText()
+    {
+        Console.WriteLine("This text is italicize....");
+    }
+
+    public void UnderLined()
+    {
+        Console.WriteLine("This text is Underlined....");
+    }
+}
+
+public class WithoutCommandPattern
+{
+    static void Main()
+    {
+        TextEditor textEditor = new TextEditor();
+
+        IButton button = new BoldButton(textEditor);
+        button.Action();
+    }
+}
---
+++ CommandPattern/GoodCode/Program.cs
@@ -0,0 +1,80 @@
+﻿public interface ICommand
+{
+    void Execute();
+}
+
+public class TextEditor
+{
+    public void BoldText()
+    {
+        Console.WriteLine("This text is bolded.....");
+    }
+
+    public void ItalicizeText()
+    {
+        Console.WriteLine("This text is italicized.....");
+    }
+
+    public void UnderlineText()
+    {
+        Console.WriteLine("This text is underlined.....");
+    }
+}
+
+public class BoldCommand : ICommand
+{
+    private TextEditor textEditor;
+
+    public BoldCommand(TextEditor textEditor)
+    {
+        this.textEditor = textEditor;
+    }
+
+    public void Execute()
+    {
+        textEditor.BoldText();
+    }
+}
+
+public class ItalicizeCommand : ICommand
+{
+    private TextEditor textEditor;
+    public ItalicizeCommand(TextEditor textEditor)
+    {
+        this.textEditor=textEditor;
+    }
+
+    public void Execute()
+    {
+        textEditor.ItalicizeText();
+    }
+}
+
+public class Button
+{
+    private ICommand command;
+    
+    public void SetCommand(ICommand command)
+    {
+        this.command = command;
+    }
+
+    public void Click()
+    {
+        command.Execute();
+    }
+}
+
+public class WithCommandPattern
+{
+    static void Main()
+    {
+        TextEditor textEditor = new TextEditor();
+
+        ICommand boldCommand = new BoldCommand(textEditor);
+
+        Button button = new Button();
+        button.SetCommand(boldCommand);
+        button.Click();
+    }
+}
